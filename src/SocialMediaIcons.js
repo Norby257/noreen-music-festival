011f7ba3965b9c78bd8d9458968e232ad08ea90c
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-10-13 20:41:12 -0500

    Header is complete

---
+++ src/SocialMediaIcons.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SocialMediaIcons = (props) => {
+    return (
+        <div style={{'float': 'left', 'background': 'black'}}> 
+
+<a href="https://linkedin.com/in/nmaddenling/" rel="noopener noreferrer" target="_blank"> <i className="fab fa-linkedin fa-2x"></i></a>
+
+<a href="https://github.com/Norby257" rel="noopener noreferrer" target="_blank"> <i className="fab fa-twitter-square fa-2x"></i></a>
+
+
+    <a href="https://github.com/Norby257" rel="noopener noreferrer" target="_blank"> <i className="fab fa-facebook-square fa-2x"></i></a>
+
+
+    <a href="https://github.com/Norby257" rel="noopener noreferrer" target="_blank"> <i className="fab fa-instagram fa-2x"></i></a>
+
+<a href="https://github.com/Norby257" rel="noopener noreferrer" target="_blank"> <i className="fab fa-reddit-square fa-2x"></i></a>
+
+        </div>
+
+    )
+
+}
+
+export default SocialMediaIcons;
